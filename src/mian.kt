054742ec03eaf3beabf15440a95e203d3e5e37e8
Edwin Dev <EdwinDeveloper@outlook.com>
2019-12-11 15:56:44 -0600

    fisrt test of kotlin

---
+++ src/mian.kt
@@ -0,0 +1,26 @@
+import java.util.*;
+
+fun main(args: Array<String>) {
+    val calendar = Calendar.getInstance();
+
+    //var nombre = nombre("Edwin",("Edwin"))
+    //println("Hola : "+nombre)
+    println("")
+
+//    if(nombre==-1){
+//        print("Hola hermano")
+//    }else{
+//        print("El valor del dato : "+nombre)
+//    }
+}
+fun otro(ejemplo : String ): String ?{
+    return ""
+}
+fun nombre(nombre: String ? , callback:(ejemplo : String)-> String ): String ?{
+    return nombre
+}
+
+
+
+
+
